0f89d08217679e0befeca24ade75926c0f19e927
Twigster <treshagaye@gmail.com>
2022-04-23 13:30:00 -0400

    updated styling

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -5,8 +5,9 @@ function Footer () {
         
     <footer className="footer bg-warning fixed-bottom">
         <h5>Lovingly created by Tresha-Gaye Ustanny</h5>
-        <a href="https://github.com/Tresha-Gaye"><img src="https://img.icons8.com/color/48/000000/github--v1.png" alt="Github" className="icon"/></a>
-        <a href="https://www.linkedin.com/in/tresha-gaye-ustanny/"><img src="https://img.icons8.com/fluent/48/000000/linkedin.png" alt="LinkedIn" className="icon"/></a>
+        <a target="_blank" rel="noreferrer" href="https://github.com/Tresha-Gaye"><img src="https://img.icons8.com/color/48/000000/github--v1.png" alt="Github" className="icon"/></a>
+        <a target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/tresha-gaye-ustanny/"><img src="https://img.icons8.com/fluent/48/000000/linkedin.png" alt="LinkedIn" className="icon"/></a>
+        <a target="_blank" rel="noreferrer" href="https://stackoverflow.com/users/17353018/tresha-gaye?tab=profile"><img src="https://img.icons8.com/color/48/000000/stackoverflow.png" alt="Stack Overflow" className="icon"/></a>
     </footer>
       
     )
